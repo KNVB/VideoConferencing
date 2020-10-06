897cff1c62485889869bd88b4b7cae4d9348f926
SITO3 <bb@cc.com>
2020-10-06 10:25:06 +0800

    Media template constraint is added.

--- client/src/utils/LocalStreamManager.js
+++ client/src/utils/LocalStreamManager.js
@@ -1,12 +1,29 @@
 class LocalStreamManager{
     constructor(){
-       
+		var templateConstraint={
+					"audio":{
+								channelCount: 2,
+								echoCancellation:true,
+								sampleSize: 16
+							},
+					"video":{
+								width:{ min: 640, ideal: 1280, max: 1920 },
+								height:{ min: 480, ideal: 720, max: 1080 }
+							}
+					}; 
         this.getMediaStream=async (shareVideo,shareAudio)=>{
             if ((shareVideo===false) && (shareAudio===false)){
                 return null;
             } else {
-                var constraints={"audio":shareAudio,"video":shareVideo};
-                console.log(constraints);   
+				//var constraints={"audio":shareAudio,"video":shareVideo};
+				var constraints={};
+				if (shareVideo){
+					constraints["video"]=templateConstraint.video;
+				}
+				if (shareAudio){
+					constraints["audio"]=templateConstraint.audio;
+				}
+				console.log(constraints);   
                 return navigator.mediaDevices.getUserMedia(constraints);
             }    
         }
